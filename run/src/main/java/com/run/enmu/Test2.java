668feda36a1f42f81654e9c17d10a6641db0e9da
lizaibiao <rungo14@163.com>
2016-07-10 15:26:23 +0800

    测试3.。。。。

--- run/src/main/java/com/run/enmu/Test2.java
+++ run/src/main/java/com/run/enmu/Test2.java
@@ -33,7 +33,7 @@ public class Test2 {
 	 */
 	public static void main(String[] args) {
 		System.out.println(EnStatus.DISABLE.getValue());
-		System.out.println("nihao2");
+		System.out.println("nio2");
 		System.out.println("nihao2");
 	}
 }
